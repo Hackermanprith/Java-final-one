722563b8b9875380c74c0dce5bb1e8a2b73a425b
Hackermanprith <prithwish.mukherjee@outlook.com>
2022-08-15 21:15:12 +0530

    LMAO

--- src/dummies/book/java/Classesstuff.java
+++ src/dummies/book/java/Classesstuff.java
@@ -2,12 +2,6 @@ package dummies.book.java;
 
 public class Classesstuff {
     public static void main(String []args){
-        String [] [] prithwish = new String[10][10];
-        for(int i = 0; i < 10;i++){
-            for(int j = 0;j<10;j++){
 
-
-            }
-        }
     }
 }
